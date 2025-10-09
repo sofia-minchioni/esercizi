493e700617f5eac2c03a46bf31448d98a74f9b9d
sofia-minchioni <sofia.minchioni@campusdavinci.edu.it>
2025-10-09 09:16:38 +0200

    fixed version 1.0.0

--- esercizio_carte_estratte.py
+++ esercizio_carte_estratte.py
@@ -1,29 +1,89 @@
-"""
-1) estrarre 3 carte da un mazzo(lista)
-[1,2,3,4,5,6,7,8,9,10,J,Q,K]
-2)calcolare punteggio usando dizionario associ a ogni carta il suo valore
-"""
-import random
-punteggio=0
-mazzo=[1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
+import pygame
+ 
+# Inizializza pygame 
+pygame.init()
+ 
+ 
+# TODO 1: crea la finestra di gioco (es. 800x600)
+LARGHEZZA=800
+ALTEZZA=600
+win = pygame.display.set_mode((800, 600)) 
+pygame.display.set_caption("Gioco con pygame.Rect")
+ 
+ 
+# Colori utili 
+WHITE = (255, 255, 255) 
+BLACK = (0, 0, 0) 
+RED = (255, 0, 0) 
+GREEN = (0, 255, 0)
+ 
+ 
+# TODO 2: crea il giocatore come oggetto pygame.Rect 
+# (usa quattro valori: x, y, larghezza, altezza) 
+player= pygame.Rect(50,250,40,40)#creo oggetto giocatore
+ 
+ 
 
-dizionario={"1":1 ,"2":2 ,"3":3,"4":4,"5":5 ,"6": 6 ,"7":7,"8":8,"9":9,"10": 10,"J": 10,"Q":10,"K":10}
+# TODO 3: crea il rettangolo obiettivo (quello verde) 
+# goal = ... 
+goal = pygame.Rect(600,250,100,100)
+ 
+ 
+# TODO 4: crea una lista con uno o due muri come pygame.Rect 
+# walls = [ ... ]
+wall=pygame.Rect(300,150,20,300)
+ 
+# Velocità del giocatore
+ 
+vel = 5 
+clock = pygame.time.Clock() 
+run = True 
+won = False
+ 
+ 
+while run: 
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            run = False
+ 
+ 
+    # TODO 5: leggi i tasti premuti 
+    keys = pygame.key.get_pressed()
+ 
+ 
+    # TODO 6: muovi il giocatore modificando player.x e player.y 
+    # ricordati di controllare le collisioni con i muri usando colliderect() 
+    if keys[pygame.K_LEFT]:
+        if player.x-vel>=0:
+            player.x -= vel
+    if keys[pygame.K_RIGHT]:
+        if player.x+player.w+vel<=LARGHEZZA:
+            player.x += vel
+        if player.colliderect(wall):
+            player.x -=vel
+            
+    if keys[pygame.K_UP]:
+        if player.y-vel>=0:
+            player.y -=vel
+ 
+ 
+    # TODO 7: controlla se il giocatore tocca il rettangolo verde (goal) 
+    # usa colliderect() per verificare la vittoria 
+    # if player.colliderect(goal): 
+    #     ...
+ if player.colliderect(goal):
+     
+ 
+    # Disegno a schermo 
+    win.fill(WHITE)
+ 
+ 
+    # TODO 8: disegna il giocatore, il traguardo e i muri
+    pygame.draw.rect(win, RED, player)
+    pygame.draw.rect(win, GREEN, goal)
+    pygame.draw.rect(win, BLACK, wall)
 
-carte_estratte = random.sample(mazzo,3)
-
-for i in range(1,3):
-   punteggio=punteggio+dizionario[carte_estratte[i]]
-    
-
-
-
-#" 2 modo"
-#import random
-
-#mazzo=[1,2,3,4,5,6,7,8,9,10,"J","Q","K"]
-
-
-#indice =random.randint(1,13)
-#carta_estratta = mazzo[indice]
-
-
+    pygame.display.update() 
+    clock.tick(60)
+ 
+pygame.quit()
